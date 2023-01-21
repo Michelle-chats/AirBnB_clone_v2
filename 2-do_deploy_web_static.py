09ca504e0154274d0abf352bc578beac1095e268
Michelle-chats <you@example.com>
2023-01-21 23:36:27 +0200

    submitting

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,77 +1,62 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+"""Generates a .tgz archive from the
+contents of the web_static folder
+Distributes an archive to a web server"""
 
-import os.path
+from fabric.operations import local, run, put
+from datetime import datetime
+import os
 from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+import re
 
-env.user = "ubuntu"
-env.hosts = ['18.204.7.186', '3.83.245.14']
+env.hosts = ['35.190.176.186', '35.196.156.157']
 
+def do_pack():
+    """Function to compress files in an archive"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+            capture=True)
+    if result.failed:
+        return None
+    return result
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-       Returns True if successful and false if not
-    """
-    if os.path.isfile(archive_path) is False:
+    """Function to distribute an archive to a server"""
+    if not os.path.exists(archive_path):
         return False
-    fullFile = archive_path.split("/")[-1]
-    folder = fullFile.split(".")[0]
-
-    # Uploads archive to /tmp/ directory
-    if put(archive_path, "/tmp/{}".format(fullFile)).failed is True:
-        print("Uploading archive to /tmp/ failed")
+    rex = r'^versions/(\S+).tgz'
+    match = re.search(rex, archive_path)
+    filename = match.group(1)
+    res = put(archive_path, "/tmp/{}.tgz".format(filename))
+    if res.failed:
         return False
-
-    # Delete the archive folder on the server
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(folder)).failed is True:
-        print("Deleting folder with archive(if already exists) failed")
+    res = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    if res.failed:
         return False
-
-    # Create a new archive folder
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(folder)).failed is True:
-        print("Creating new archive folder failed")
+    res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(filename, filename))
+    if res.failed:
         return False
-
-    # Uncompress archive to /data/web_static/current/ directory
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(fullFile, folder)).failed is True:
-        print("Uncompressing archive to failed")
+    res = run("rm /tmp/{}.tgz".format(filename))
+    if res.failed:
         return False
-
-    # Deletes latest archive from the server
-    if run("rm /tmp/{}".format(fullFile)).failed is True:
-        print("Deleting archive from /tmp/ directory dailed")
-        return False
-
-    # Move folder from web_static to its parent folder,to expose the index
-    # files outsite the /we_static path
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".
-           format(folder, folder)).failed is True:
-        print("Moving content to archive folder before deletion failed")
-        return False
-
-    # Delete the empty web_static file, as its content have been moved to
-    # its parent directory
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(folder)).failed is True:
-        print("Deleting web_static folder failed")
-        return False
-
-    # Delete current folder being served (the symbolic link)
-    if run("rm -rf /data/web_static/current").failed is True:
-        print("Deleting 'current' folder failed")
-        return False
-
-    # Create new symbolic link on web server linked to new code version
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(folder)).failed is True:
-        print("Creating new symbolic link to new code version failed")
-        return False
-
-    print("New version deployed!")
-    return True
+    res = run("mv /data/web_static/releases/{}"
+            "/web_static/* /data/web_static/releases/{}/"
+            .format(filename, filename))
+    if res.failed:
+         return False
+     res = run("rm -rf /data/web_static/releases/{}/web_static"
+             .format(filename))
+     if res.failed:
+         return False
+     res = run("rm -rf /data/web_static/current")
+     if res.failed:
+         return False
+     res = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+             .format(filename))
+     if res.failed:
+         return False
+     print('New version deployed!')
+     return True
+    
