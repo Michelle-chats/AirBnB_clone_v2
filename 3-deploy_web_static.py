aa5edbfaa95ef3f3ab274d687fcc0914394ecb26
Michelle-chats <you@example.com>
2023-01-21 21:15:46 +0200

    submitting

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,55 +1,16 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-@author: kgomera
+Pack tarball and deploy to servers
 """
-from fabric.api import local, put, run, env
-from datetime import datetime
-
-env.user = 'ubuntu'
-env.hosts = ['35.227.35.75', '100.24.37.33']
-
-
-def do_pack():
-    """
-    Targging project directory into a packages as .tgz
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('sudo mkdir -p ./versions')
-    path = './versions/web_static_{}'.format(now)
-    local('sudo tar -czvf {}.tgz web_static'.format(path))
-    name = '{}.tgz'.format(path)
-    if name:
-        return name
-    else:
-        return None
-
-
-def do_deploy(archive_path):
-    """Deploy the boxing package tgz file
-    """
-    try:
-        archive = archive_path.split('/')[-1]
-        path = '/data/web_static/releases/' + archive.strip('.tgz')
-        current = '/data/web_static/current'
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
-        run('rm /tmp/{}'.format(archive))
-        run('mv {}/web_static/* {}'.format(path, path))
-        run('rm -rf {}/web_static'.format(path))
-        run('rm -rf {}'.format(current))
-        run('ln -s {} {}'.format(path, current))
-        print('New version deployed!')
-        return True
-    except:
-        return False
+do_pack = __import__('1-pack_web_static')
+do_deploy = __import__('2-do_deploy_web_static')
 
 
 def deploy():
     """
-    A function to call do_pack and do_deploy
+    Pack tarball and deploy to servers
     """
-    archive_path = do_pack()
-    answer = do_deploy(archive_path)
-    return answer
+    filepath = do_pack.do_pack()
+    if not filepath:
+        return False
+    return do_deploy.do_deploy(filepath)
